54d6f4a55b74bfd6a53d3680e343595f908cf8ab
matinrostami1060-art <matinrostami1060@gmail.com>
2026-04-30 14:20:39 +0330

    Delete relay.js

--- relay.js
+++
@@ -1,9 +0,0 @@
-export default async (request) => {
-  const url = new URL(request.url);
-  url.protocol = "http:";
-  url.hostname = "vpn.matsa12345.ir";
-  url.port = "8080";
-  const headers = new Headers(request.headers);
-  headers.set("host", "vpn.matsa12345.ir");
-  return await fetch(url.toString(), { method: request.method, headers, body: request.body, redirect: "manual" });
-};
